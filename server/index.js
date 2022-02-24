028957756f5ed1b5795c18e2a39a22c03bd81f6a
leduong290300 <leduong290300@gmail.com>
2022-02-24 17:56:30 +0700

    fix-02

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
+// const cors = require("cors");
 const UserRouter = require("./Router/UserRouter");
 const PostRouter = require("./Router/PostRouter");
 const connectDB = require("./Config/database");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 
 connectDB();
 const PORT = process.env.PORT || 5000;
