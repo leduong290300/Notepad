293fe3cd4f95fbaae62181949ea7ecd9906fd1da
leduong290300 <leduong290300@gmail.com>
2022-02-25 10:13:29 +0700

    fix-06

--- server/index.js
+++ server/index.js
@@ -7,14 +7,19 @@ const connectDB = require("./Config/database");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.REACT_URL);
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-  next();
-});
-
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", process.env.REACT_URL);
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+//   next();
+// });
+app.use(
+  cors({
+    origin: process.env.REACT_URL,
+    credentials: true,
+  }),
+);
 connectDB();
 app.use(express.json());
 
