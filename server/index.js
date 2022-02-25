0b527d0560c4303cc1373f7941f87cc3e239fdea
leduong290300 <leduong290300@gmail.com>
2022-02-25 09:32:31 +0700

    fix-04

--- server/index.js
+++ server/index.js
@@ -1,31 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
-// const cors = require("cors");
+const cors = require("cors");
 const UserRouter = require("./Router/UserRouter");
 const PostRouter = require("./Router/PostRouter");
 const connectDB = require("./Config/database");
 dotenv.config();
 const app = express();
-app.use(express.json());
-const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With",
-  );
-
-  // intercept OPTIONS method
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
 
 connectDB();
 const PORT = process.env.PORT || 5000;
-app.use(allowCrossDomain);
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/user", UserRouter);
 app.use("/api/posts", PostRouter);
